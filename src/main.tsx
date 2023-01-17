9cff59726ebde5602449a032a92c94d67ef15439
yong liu <564447023@qq.com>
2023-01-18 00:10:22 +0800

    feat: implement virtual-module plugin

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,13 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App'
-import './index.css'
-
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import App from './App';
+import './index.css';
+import fib from 'virtual:fib';
+import env from 'virtual:env';
+console.log(env);
+alert(`结果: ${fib(10)}`);
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
     <App />
-  </React.StrictMode>,
-)
+  </React.StrictMode>
+);
